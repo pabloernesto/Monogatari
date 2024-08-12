97759ec7aec63adaa7d13f0783622e66ee57135c
Pablo Ernesto Repetto <pablo.ernesto.repetto@gmail.com>
2024-08-12 15:27:54 -0300

    wip: Implement pause and speed typing commands

--- src/lib/MonoTyped.js
+++ src/lib/MonoTyped.js
@@ -16,22 +16,50 @@ export default class Typed {
 		return this.config.strings;
 	}
 
+	/**
+		@param {HTMLElement} element
+		@param {string} curString
+	 */
 	setDisplay(element, curString) {
 		let nodes = [];
-		// iterate over the string, adding <span>s to the element as we go
-		for (const char of curString) {
-			const textNode = document.createTextNode(char);
-			let node;
-			const isWhite = /\s/.test(char);
-			if (isWhite) {
-				node = textNode;
+		let actions = [];
+		let visibleCharsSeenN = 0;
+
+		// Separate curString into text and action sections
+		//   eg: "{speed:10}hello {pause:1000}{speed:1000}world!"
+		//     -> [ '', '{speed:10}', 'hello ', '{pause:1000}', '', '{speed:1000}', 'world!' ]
+		// `(?:<pattern>)` is a non-capturing group, see https://devdocs.io/javascript/regular_expressions/non-capturing_group
+		const actionPattern = /\{(?:pause|speed):\d+\}/;
+		const sections = curString.split(actionPattern);
+
+		sections.forEach((section, i) => {
+			// text section
+			if (i % 2 === 0) {
+				// iterate over the string, adding <span>s to the element as we go
+				for (const char of section) {
+					const textNode = document.createTextNode(char);
+					let node;
+					const isWhite = /\s/.test(char);
+					if (isWhite) {
+						node = textNode;
+					} else {
+						visibleCharsSeenN++;
+						node = document.createElement('span');
+						node.append(textNode);
+						node.style.visibility = 'hidden';
+					}
+					nodes.push(node);
+				}
+			// action section
 			} else {
-				node = document.createElement('span');
-				node.append(textNode);
-				node.style.visibility = 'hidden';
+				const match = /\{(?<action>pause|speed):(?<n>\d+)\}/.exec(section);
+				actions[visibleCharsSeenN] = {
+					action: match.groups.action,
+					n: match.groups.n,
+				};
 			}
-			nodes.push(node);
-		}
+		});
+
 		this.el.replaceChildren(...nodes);
 	}
 
